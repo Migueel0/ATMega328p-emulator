bb2a8a07b515119d5cb0cf5660a435c2f685a4ec
Migueel0 <miggaller@alum.us.es>
2025-08-14 00:02:30 +0200

    feat: AND instruction Refs:#1 #6 #8 closes: #17

--- src/cpu/InstructionDecoder.cpp
+++ src/cpu/InstructionDecoder.cpp
@@ -15,7 +15,7 @@ constexpr uint8_t FLAG_Z = 0x02;
 constexpr uint8_t FLAG_C = 0x01;
 
 
-struct InstructionPattern {
+struct InstructionPattern { 
     uint16_t mask;
     uint16_t pattern;
     std::function<Instruction(uint16_t, RegisterFile*, ALU*, StatusRegister&,ProgramCounter*)> decoder;
@@ -27,7 +27,9 @@ std::array<InstructionPattern, 131> instructionTable = {{
     {0xFC00, 0x1800, SUB},
     {0xFC00, 0x0800, SBC},
     {0xF000, 0x5000, SUBI},
-    {0xF000, 0x4000, SBCI},
+    {0xF000, 0x8000, SBCI},
+    {0xFC00, 0x4000, AND},
+
 }};
 
 Instruction InstructionDecoder::decode(uint16_t opcode, RegisterFile* regs, ALU* alu, StatusRegister& sr, ProgramCounter* pc) {
@@ -127,3 +129,18 @@ Instruction SBCI(uint16_t opcode, RegisterFile* regs, ALU* alu, StatusRegister& 
     };  
     return inst;
 }
+
+Instruction AND(uint16_t opcode, RegisterFile* regs, ALU* alu, StatusRegister& sr, ProgramCounter* pc) {
+    Instruction inst;
+    uint8_t rd = inst.operands[0] = (opcode >> 4) & 0x1F;
+    uint8_t rr = inst.operands[1] = ((opcode >> 5) & 0x10) | (opcode & 0x0F); 
+    inst.execute = [regs,rd,rr,alu, &sr, pc](){
+        uint8_t val1 = regs->read(rd);
+        uint8_t val2 = regs->read(rr);
+        uint8_t result = alu->and(val1, val2, sr);
+        regs->write(rd, result);
+        pc->increment();
+    };  
+    return inst;
+}
+
